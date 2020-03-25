a8b70191a0eab7c17bf4384fd8fb748c73addb4e
MadsonSousa <madson_ss_10@hotmail.com>
2020-03-24 23:09:36 -0300

    Update _settings_base.py

--- market_maker/_settings_base.py
+++ market_maker/_settings_base.py
@@ -10,8 +10,8 @@ BASE_URL = "https://testnet.bitmex.com/api/v1/"
 # BASE_URL = "https://www.bitmex.com/api/v1/" # Once you're ready, uncomment this.
 
 # The BitMEX API requires permanent API keys. Go to https://testnet.bitmex.com/app/apiKeys to fill these out.
-API_KEY = "x8PTL5JBpKBTP2wk2FNA86OB"
-API_SECRET = "hikYXMd1PHVKl7dLlmTHoxdDXCBIGzKJQmaSgzdRjkBy6Jc-"
+API_KEY = ""
+API_SECRET = ""
 
 
 ########################################################################################################################
